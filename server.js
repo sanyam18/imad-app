0932f1c5d52a4df302a85fdfe71790052791cc0f
sanyam18 <30959275+sanyam18@users.noreply.github.com>
2017-08-13 02:16:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/article-one',function (req, res)
 {
-    res.send('here article one will be served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one'.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
